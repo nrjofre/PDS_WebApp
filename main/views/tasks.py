d69a1908c42a7dba474c31a0a1e3ed2018acefe2
Macaya25 <afmacaya@miuandes.cl>
2022-09-12 05:23:08 -0300

    Tasks are shown in '{url}/tasks'.

--- main/views/tasks.py
+++ main/views/tasks.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from ..models.task import Task
+from main.models import task 
 
 class Tasks(View):
     def get(self, request):
-        context = {}
+        tasks_list = Task.objects.all()
+        context = {'task_list': tasks_list}
         return render(request, 'tasks.html', context)
